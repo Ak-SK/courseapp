4c0e31a3f95c85f6ffc0fb3203bfe4fdeaebc09e
kittivasal <87867875+kittivasal@users.noreply.github.com>
2021-07-23 21:20:30 +0530

    firstcommit

---
+++ src/Components/Institution/Institution.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Institution = () => {
+  return (
+    <>
+      <h1>Institution Component</h1>
+    </>
+  );
+};
+export default Institution;
---
+++ src/Components/Signin/Signin.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Signin = () => {
+  return (
+    <>
+      <h1>Signin Component</h1>
+    </>
+  );
+};
+export default Signin;
